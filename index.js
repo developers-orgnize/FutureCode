13711877a6b778876901b6eea7233d58d53f63fb
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-10-12 13:36:42 -0700

    product id

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Product = require('./models/Product.Model');
 const app = express();
 
+
 app.use(express.json());
 
 // back-end  is running port 3000 
@@ -13,7 +14,7 @@ app.get('/', async (req, res) => {
     res.send(" hello from node api sandalu thushan ");
 })
 
-// product model
+// product model 
 
 
 // app.post ("/api/products",(req,res)=>{
@@ -23,6 +24,33 @@ app.get('/', async (req, res) => {
 
 // });
 
+
+// GET route to fetch all products
+app.get('/api/products', async (req, res) => {
+    try {
+        // Retrieve all products from the database
+        const products = await Product.find({}); // Find all products
+
+        // Respond with the list of products
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Define GET route to get a product by ID
+app.get('/api/products/:id', async (req, res) => {
+    const { id } = req.params; // Get the ID from the request parameters
+    try {
+        const product = await Product.findById(id); // Find the product by ID
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' }); // Return 404 if not found
+        }
+        res.status(200).json(product); // Return the found product
+    } catch (error) {
+        res.status(500).json({ message: error.message }); // Handle any errors
+    }
+});
 
 app.post('/api/products', async (req, res) => {
     try {
@@ -43,6 +71,11 @@ app.post('/api/products', async (req, res) => {
   });
   
 //nodemon use for when refreshing web site thi application shlould be in this method
+
+
+//get meetgod--> retrival data from the mongo db 
+
+
 
 
 mongoose.connect('mongodb+srv://user1:Thush12213@cluster0.9qwykfs.mongodb.net/nodedb?retryWrites=true&w=majority&appName=Cluster0')
